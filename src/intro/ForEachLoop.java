b8f961e03a52218f15396c403c25f32056b77403
Adejumok <roseadeyinka01@gmail.com>
2023-07-13 10:51:21 +0100

    updated commit

--- src/intro/ForEachLoop.java
+++ src/intro/ForEachLoop.java
@@ -1,17 +1,19 @@
+package intro;
+
 public class ForEachLoop {
     public static void main(String[] args) {
 ////        Declaring an array
-//        int[] arr = {12, 23, 44, 56, 78};
-////        printing array using for=each loop
-//        for(int i:arr){
-////            System.out.println(i);
-//            System.out.print(i + ",");
-//        }
+        int[] arr = {12, 23, 44, 56, 78};
+//        printing array using for=each loop
+        for(int i:arr){
+            System.out.println(i);
+            System.out.print(i + ",");
+        }
 
 //        infinitive for loop
 //        using no condition in for loop
-        for (;;){
-            System.out.println("infinitive loop");
-        }
+//        for (;;){
+//            System.out.println("infinitive loop");
+//        }
     }
 }
